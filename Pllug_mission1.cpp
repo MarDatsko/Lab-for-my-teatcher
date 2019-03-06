e0fe9e1f18e5788d05382aecde052c0b92f3ce13
MarDatsko <datskomar@gmail.com>
2019-03-06 23:46:39 +0200

    Two cycle,not error

---
+++ Pllug_mission1.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	string choice_user;
+	int user,comp;
+	do
+	{
+		cout << "Enter [Rock], [Scissors], [Paper]" << endl;
+		cout << "EXIT enter [x]" << endl;
+		cin >> choice_user;
+		comp = rand() % 3;
+		if (choice_user == "x")
+		{
+			break;
+		}
+		do
+		{
+			if (choice_user == "rock" || choice_user == "Rock")
+			{
+				user = 0;
+			}
+			else if (choice_user == "scissors" || choice_user == "Scissors")
+			{
+				user = 1;
+			}
+			else if (choice_user == "paper" || choice_user == "Paper")
+			{
+				user = 2;
+			}
+			else
+			{
+				cout << "Incorect word. Try Again";
+				break;
+			}
+
+			if (comp == 0)
+			{
+				cout << "Rock";
+			}
+			else if (comp == 1)
+			{
+				cout << "Scissors";
+			}
+			else if (comp == 2)
+			{
+				cout << "Paper";
+			}
+
+			cout << endl;
+			if (user == comp)
+			{
+				cout << "WIN FRIENDSHIP";
+				break;
+			}
+			else if (user > comp)
+			{
+				cout << "YOU WIN";
+				break;
+			}
+			else if (user < comp)
+			{
+				cout << "YOU LOSS";
+				break;
+			}
+			
+		} while (true);
+		cout << endl << endl;
+	} while (true);
+	_getch();
+	return 0;
+}
