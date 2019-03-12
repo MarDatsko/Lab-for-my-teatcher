753840a162a0a42e875c9184f406eadac95f5fa6
MarDatsko <datskomar@gmail.com>
2019-03-12 23:35:21 +0200

    It works,but I have some questions

---
+++ Add_or_Delete_in_arrey.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+void PrintArrey(int *_yura, int size_arrey)
+{
+	for (int i = 0; i < size_arrey; i++)
+	{
+		cout << _yura[i] << " ";
+	}
+}
+
+void AddArrey(int *_yura, int size_arrey,int x)
+{
+	int *yura1 = new int[size_arrey + x];
+	for (int i = 0; i < size_arrey; i++)
+	{
+		yura1[i] = _yura[i];
+	}
+	for (int i = size_arrey; i < (size_arrey + x); i++)
+	{
+		yura1[i] = rand()% 234;
+	}
+	PrintArrey(yura1, (size_arrey + x));
+}
+
+void DeleteArrey(int *_yura, int size_arrey, int x)
+{
+	int *yura1 = new int[size_arrey - x];
+	for (int i = 0; i < (size_arrey - x); i++)
+	{
+		yura1[i] = _yura[i];
+	}
+	PrintArrey(yura1, (size_arrey - x));
+}
+
+void RandomArrey(int *_yura, int size_arrey)
+{
+	for (int i = 0; i < size_arrey; i++)
+	{
+		_yura[i] = rand() % 123;
+	}
+}
+
+int main()
+{	
+	int SizeArrey,x;
+	char choice_user;
+	cout << " Enter the arrey size ";
+	cin >> SizeArrey;
+	int *yura = new int[SizeArrey];
+	RandomArrey(yura, SizeArrey);
+	PrintArrey(yura, SizeArrey);
+	cout << endl << "Add number [a] Delete number [d]";
+	cin >> choice_user;
+	if (choice_user == 'a' || choice_user == 'A')
+	{
+		cout << "Enter number what you wont add ";
+		cin >> x;
+		AddArrey(yura,SizeArrey,x);
+	}
+	else if (choice_user == 'd' || choice_user == 'D')
+	{
+		cout << "Enter number what you wont delete ";
+		cin >> x;
+		DeleteArrey(yura, SizeArrey, x);
+	}
+	else
+	{
+		cout << " Error/ Try again/ =) ";
+	}
+	system("pause");
+	return 0;
+}
